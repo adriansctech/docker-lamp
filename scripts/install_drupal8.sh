5ef1fcdfb8b4197b9e3f8066e2b07e529e77bb4f
Adri Sanchez Carchano <adriansctech@3558582.no-reply.drupal.org>
2020-04-10 18:58:22 +0200

    Include tail -f in apache log and open browser with localhost url when start docker container

--- scripts/install_drupal8.sh
+++ scripts/install_drupal8.sh
@@ -2,7 +2,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
-
+rm ../www/.keep
 docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/000-default.conf"
 docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/default-ssl.conf"
 docker-compose exec --user=1000:1000 web sh -c "composer create-project drupal/recommended-project /var/www/html/"
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker-compose up -d
+xdg-open http:localhost && tail -f apache-logs/error.log
