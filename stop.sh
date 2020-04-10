9f5c8917fe611ea188f3d5de513b39ee79815030
Adri Sanchez Carchano <adriansctech@3558582.no-reply.drupal.org>
2020-04-10 19:09:44 +0200

    Include logs in other folder when stop docker

--- stop.sh
+++ stop.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "*********************************************"
+echo "******create backup of apache logs now ******"
+echo "*********************************************"
+mv apache-logs/access.log apache-logs/$( date '+%Y-%m-%d' )-access.log
+mv apache-logs/error.log apache-logs/$( date '+%Y-%m-%d' )-error.log
 docker-compose stop
