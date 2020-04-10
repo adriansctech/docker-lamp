0f1794aa8d74a7bf20057a4271a5cc041d06af25
Adri Sanchez Carchano <adriansctech@3558582.no-reply.drupal.org>
2020-04-10 15:44:20 +0200

    Remove last line to create symbolic linc

--- scripts/install_drupal8.sh
+++ scripts/install_drupal8.sh
@@ -5,13 +5,6 @@ cd "$parent_path"
 
 docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/000-default.conf"
 docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/default-ssl.conf"
-rm -rf ../www;
-cd .. && composer create-project drupal-composer/drupal-project:8.x-dev www
-./stop.sh
-./start.sh
+docker-compose exec --user=1000:1000 web sh -c "composer create-project drupal/recommended-project /var/www/html/"
+docker-compose exec --user=1000:1000 web sh -c "composer require drush/drush"
 docker-compose exec --user=1000:1000 web sh -c 'vendor/bin/drush si standard --db-url=mysql://root:123@db/drupal8 -y'
-docker-compose exec web sh -c "ln -s /var/www/html/vendor/bin/dush /usr/local/bin/drush"
-
-#work in here
-#docker-compose exec --user=1000:1000 web sh -c "composer create-project drupal/recommended-project /var/www/html/"
-#docker-compose exec --user=1000:1000 web sh -c "composer require drush/drush"
