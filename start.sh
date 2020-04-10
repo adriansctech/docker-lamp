41c79300354dcc44fc8d6b8b45975367eb6f6238
Adri Sanchez Carchano <adriansctech@3558582.no-reply.drupal.org>
2020-04-10 20:21:27 +0200

    DIsplay in real time error logs/access of apache, and create backup of this files

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 docker-compose up -d
-xdg-open http:localhost && tail -f apache-logs/error.log
+if [ -s apache-logs/error.log ]
+then
+		echo "***************************************"
+		echo "**** Display error log in real time ***"
+		echo "****************************************"
+        tail -f apache-logs/error.log
+        xdg-open http:localhost
+else
+		echo "****************************************"
+		echo "**** Display access log in real time ***"
+		echo "****************************************"
+    	tail -f apache-logs/access.log
+    	xdg-open http:localhost
+fi
--- stop.sh
+++ stop.sh
@@ -2,6 +2,9 @@
 echo "*********************************************"
 echo "******create backup of apache logs now ******"
 echo "*********************************************"
-mv apache-logs/access.log apache-logs/$( date '+%Y-%m-%d' )-access.log
-mv apache-logs/error.log apache-logs/$( date '+%Y-%m-%d' )-error.log
+pkill -9 tail
+cp apache-logs/access.log apache-logs/$( date '+%Y-%m-%d-%H-%M' )-access.log
+cp apache-logs/error.log apache-logs/$( date '+%Y-%m-%d-%H-%M' )-error.log
+yes | mv apache-logs/????-??-??-??-??-access.log apache-logs/old-logs/
+yes | mv apache-logs/????-??-??-??-??-error.log apache-logs/old-logs/
 docker-compose stop
